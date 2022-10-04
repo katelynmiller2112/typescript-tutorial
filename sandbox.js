03a3a3aba2df4f9c816f8d6629ef651f4c8f471a
katelynmiller2112 <katelynmiller@outlook.com>
2022-10-03 20:45:27 -0400

    made changes to follow along with tutorial

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,11 @@
-var character = 'mario';
+// creating a const name mario, this is now a string
+var character = 'luigi';
+// can now output const to console
 console.log(character);
+// interacting with dom using the query selector just like in vanilla js
 var inputs = document.querySelectorAll('input');
 inputs.forEach(function (input) {
     console.log(input);
 });
+// sudo tsc filename.ts -w  
+// this sets up watch command to live re-compile when file saved w/ changes
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,16 @@
-const character = 'mario';
 
+// creating a const name mario, this is now a string
+const character = 'luigi';
+
+// can now output const to console
 console.log(character);
 
+// interacting with dom using the query selector just like in vanilla js
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach(input => {
   console.log(input);
 });
+
+// sudo tsc filename.ts -w  
+// this sets up watch command to live re-compile when file saved w/ changes
